196164d9c2e968e8232f50450591fdf092f50b3a
Jake Miller <jake0miller@gmail.com>
2019-11-04 11:44:55 -0700

    Test Olympian model

--- app/models/olympian.rb
+++ app/models/olympian.rb
@@ -1,4 +1,7 @@
 class Olympian < ApplicationRecord
+  validates_presence_of :name, :sex, :age, :height,
+                       :weight, :team, :sport
+
   has_many :olympian_events
   has_many :events, through: :olympian_events
 end
